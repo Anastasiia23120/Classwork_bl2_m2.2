4138646d5ec17a3731564c432183d104b768f23d
Anastasiia Medved <nastya0662793393@gmail.com>
2024-12-17 02:58:39 +0100

    last part

---
+++ js/02-loops.js
@@ -0,0 +1,81 @@
+//* part 1
+// let a = 10;
+// // const b = ++a;
+// const b = --a;
+
+// a = a + 1;
+// a += 1;
+// a++;
+// ++a;
+
+// a = a - 1;
+// a -= 1;
+// a--;
+// --a;
+
+// console.log(a);
+// console.log(b);
+
+//* part 2
+// let i = 1;
+
+// while (i <= 50) {
+//   console.log(i);
+
+//   i++;
+// }
+
+//* part 3
+// let userNumber;
+
+// do {
+//   userNumber = Number(prompt('Введіть число більше за 100'));
+// } while (userNumber <= 100);
+
+// console.log(userNumber);
+
+//* part 4
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//* part 5
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('FizzBuzz');
+//   } else if (i % 3 === 0) {
+//     console.log('Fizz');
+//   } else if (i % 5 === 0) {
+//     console.log('Buzz');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//* task 1
+
+// const a = 20;
+// const b = 100;
+
+// for (let i = a; i <= b; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+//* task 2
+
+// const min = 0;
+// const max = 5;
+
+// let total = 0;
+
+// for (let i = min; i <= max; i++) {
+//   if (i % 2 === 0) {
+//     total += i;
+//   }
+// }
+
+// console.log(total);
